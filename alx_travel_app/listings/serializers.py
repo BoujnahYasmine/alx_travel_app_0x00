2723d951e68e56ca9c20db4e803678d706528912
Yasmine Boujnah <132377962+BoujnahYasmine@users.noreply.github.com>
2025-07-20 19:42:12 +0100

    Update serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
-from .models import Listing, Booking
+from .models import Listing, Booking, Review
 
 class ListingSerializer(serializers.ModelSerializer):
-    """Serialize Listing model"""
     class Meta:
         model = Listing
-        fields = '__all__'
-
+        fields = ['id', 'title', 'description', 'location', 'price_per_night', 'max_guests',
+                  'host', 'created_at']
+        read_only_fields = ['created_at']
 
 class BookingSerializer(serializers.ModelSerializer):
-    """	Serialize Booking model"""
     class Meta:
         model = Booking
         fields = '__all__'
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ['created_at']
